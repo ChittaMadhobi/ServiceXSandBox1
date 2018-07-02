ed388e71c2b540fcaf5eb3f19f985e86b2bf1717
Jit <sarbojit.project@gmail.com>
2018-07-01 17:01:34 -0700

    Completed for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 // const keys = require('./config/keys');
 
@@ -33,6 +34,18 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+//=============== Server STATIC ASSETS if in production ========
+if (process.env.NODE_ENV === 'production') {
+  // Set a static folder and point to client side, build as
+  app.use(express.static(client / build));
+  // client is the client-directory or client-side and 'build' is the build
+  // directory created via building the client side (check client build in deploy doc)
+  // In reality, we would ask heroku to do the build and not locally build it
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 //=======TEST CONN===============================================
 // app.get('/', (req, res) => {
 //   res.send('Node Server Check from Browser ... SUCCESS');
